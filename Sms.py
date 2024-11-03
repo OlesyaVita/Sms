02d5cd17a212bf9eed5b35308ac6bb83ee0885db
KustovyKOAL <lesyavita@ya.ru>
2024-11-04 02:04:08 +0300

    Добавлена функция сохранения электронных адресов и пароля в файл save.txt

--- Sms.py
+++ Sms.py
@@ -2,7 +2,17 @@ from tkinter import *
 import smtplib
 from email.message import EmailMessage
 
+
+def save():
+    with open("save.txt", "w") as file:
+        file.write(sender_email_entry.get() + '\n')
+        file.write(recipient_email_entry.get() + '\n')
+        file.write(password_entry.get() + '\n')
+
+
+
 def send_email():
+    save()
     sender_email = sender_email_entry.get()
     recipient_email = recipient_email_entry.get()
     password = password_entry.get()  # пароль с Безопасности в Настройках почты
@@ -33,27 +43,27 @@ window = Tk()
 window.title("Отправка Email")
 window.geometry("500x300")
 # Создание и размещение виджетов
-Label(text="Отправитель (Email):").grid(row=0, column=0, sticky=W)
+Label(text="Отправитель (Email): ").grid(row=0, column=0, sticky=W)
 sender_email_entry = Entry()
 sender_email_entry.grid(row=0, column=1)
 
-Label(text="Получатель (Email):").grid(row=1, column=0, sticky=W)
+Label(text="Получатель (Email): ").grid(row=1, column=0, sticky=W)
 recipient_email_entry = Entry()
 recipient_email_entry.grid(row=1, column=1)
 
-Label(text="Пароль приложения:").grid(row=2, column=0, sticky=W)
+Label(text="Пароль приложения: ").grid(row=2, column=0, sticky=W)
 password_entry = Entry(show="*")
 password_entry.grid(row=2, column=1)
 
-Label(text="Тема:").grid(row=3, column=0, sticky=W)
+Label(text="Тема: ").grid(row=3, column=0, sticky=W)
 subject_entry = Entry()
 subject_entry.grid(row=3, column=1)
 
-Label(text="Сообщение:").grid(row=4, column=0, sticky=W)
+Label(text="Сообщение: ").grid(row=4, column=0, sticky=W)
 body_text = Text(height=10, width=45)
 body_text.grid(row=4, column=1)
 
-Button(text="Отправить", command=send_email).grid(row=5, column=1, sticky=W)
+Button(text="Отправить письмо", command=send_email).grid(row=5, column=1, sticky=W)
 
 result_label = Label(text="")
 result_label.grid(row=6, column=1, sticky=W)
